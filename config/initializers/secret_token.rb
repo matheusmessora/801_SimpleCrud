21a7237a28241c231189100713dd45d571bd3acd
Matheus Messora <matheus.messora.vpn@gmail.com>
2013-01-23 13:06:06 -0200

    Generate first SCAFFOLD - User

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCrud::Application.config.secret_token = '2687d48db69fac5a05c9cfb40c1a038a592202cce16f967bad1617f602e9919fedaea86b5bdc7adcf2148bc2cd2356d7cdd5ef21fcbeb56230d1d3987a064756'
