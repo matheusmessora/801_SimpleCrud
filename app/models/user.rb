d8ae7e2766ac4755b8731e1c90a1ca352d074d64
Matheus Messora <matheus.messora.vpn@gmail.com>
2013-01-23 18:59:49 -0200

    Validacao do USER

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
+# encoding: utf-8
 class User < ActiveRecord::Base
-  attr_accessible :email, :login, :name, :password
+	attr_accessible :email, :login, :name, :password
+
+	#presence
+	validates	:email, :login, :password, :presence => true
+	#length
+	validates	:email, :length => { :minimum => 5, :message => "muito curto" }
+	#regex
+	validates	:email, :inclusion => { :in => %w(@), :message => "inválido" }
+
 end
